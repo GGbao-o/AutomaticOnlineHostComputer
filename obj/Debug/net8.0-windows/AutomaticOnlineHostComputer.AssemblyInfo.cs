5586b89307677589e40e88fea1ac08ab1e5f35e3
ggbao1236 <3093577348@qq.com>
2026-04-27 13:36:05 +0800

    13.36

--- obj/Debug/net8.0-windows/AutomaticOnlineHostComputer.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/AutomaticOnlineHostComputer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutomaticOnlineHostComputer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a13ad40446af4ee87b371707f5bb5f205c7063fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d451685ae38a134e67a77d071a7bef05f55cca7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutomaticOnlineHostComputer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutomaticOnlineHostComputer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
