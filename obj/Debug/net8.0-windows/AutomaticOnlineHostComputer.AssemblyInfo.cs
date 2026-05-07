c3ace2339be943876d16d7078bf48f085e6eff83
ggbao1236 <3093577348@qq.com>
2026-05-07 14:04:13 +0800

    连接并且读取数据正常 1号线天车前
               XPos=182059008, YPos=644, ZPos=-722
               UI：已连接，就绪 | 无任务 | 模式未知(0) | 运行条件满足 | 无版
               研磨机天车
    
               XPos=-1499529217, YPos=2, ZPos=10
               UI：已连接，就绪 | 无任务 | 模式未知(0) | 运行条件满足 | 无版

--- obj/Debug/net8.0-windows/AutomaticOnlineHostComputer.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/AutomaticOnlineHostComputer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutomaticOnlineHostComputer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc291596dded04874542def65e5efc42c611cfa8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38f63411b96c9557c8c23470989d96c61bb7bace")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutomaticOnlineHostComputer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutomaticOnlineHostComputer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
