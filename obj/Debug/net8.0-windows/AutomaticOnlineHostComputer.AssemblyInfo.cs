3fd94576e82f4593cfd060eeb9f5d9a267ed02bc
ggbao1236 <3093577348@qq.com>
2026-05-09 14:18:03 +0800

    更细寄存器地址数值 PLC 侧这些寄存器 0 是常态，2 是触发指令。如果一直保持 2，PLC 会认为指令一直在发。写完 2 立刻回 0，形成一次脉冲，PLC 只响应一次。

--- obj/Debug/net8.0-windows/AutomaticOnlineHostComputer.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/AutomaticOnlineHostComputer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutomaticOnlineHostComputer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f9b50cee5f229471a38b7b3f178227f8b22c910")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f19ec36b24850a3b5c14fdceb6b54f6d8a6491f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutomaticOnlineHostComputer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutomaticOnlineHostComputer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
